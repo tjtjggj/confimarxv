889e8516c1a4a3576b33db967ecd285c9940bc14
tjtjggj <radl060418@gmail.com>
2023-11-17 23:31:41 -0600

    first commit

---
+++ php/conexion_be.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$conexion = mysqli_connect("localhost", "root", "","invitados_fiesta");
+/*
+if($conexion){
+    echo 'Conectado exitosamente a la Base de Datos';
+}else{
+    echo 'No se ha podido conectar a la Base de Datos';
+}
+*/
+
+?>
